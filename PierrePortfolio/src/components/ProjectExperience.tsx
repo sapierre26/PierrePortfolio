fcc455a6b1b1c0cd0eb70bb016dd4e26a7e052c5
Sanaia Pierre <122914227+sapierre26@users.noreply.github.com>
2026-08-02 15:01:53 -0500

    Added styling and formatting to ProjectExperience and ProjectItem components; Still need to do the slider for the projects

--- PierrePortfolio/src/components/ProjectExperience.tsx
+++ PierrePortfolio/src/components/ProjectExperience.tsx
@@ -1,13 +1,18 @@
-// import ProjectItem from './ProjectItem.tsx'
+import '../css/tokens.css'
+import '../css/ProjectExperience.css'
+import ProjectItem from './ProjectItem.tsx'
 
 function Projects() {
   return (
     <>
-      <section id="projects-section">
+      <section className="container" id="projects-section">
         <h1>PROJECT EXPERIENCE</h1>
 
         <div id="project-horizontal-slider">
-          {/* <ProjectItem /> */}
+          <ProjectItem />
+          <ProjectItem />
+          <ProjectItem />
+          <ProjectItem />
         </div>
       </section>
     </>
--- PierrePortfolio/src/components/ProjectItem.tsx
+++ PierrePortfolio/src/components/ProjectItem.tsx
@@ -1,24 +1,24 @@
-import linkedinLogo from './assets/icons/linkedin.svg'
-import emailLogo from './assets/icons/email.svg'
-import githubLogo from './assets/icons/github.svg'
-import siteLinkLogo from './assets/icons/site_link.svg'
+import '../css/tokens.css'
+import '../css/ProjectExperience.css'
+import linkedinLogo from '../assets/icons/linkedin.svg'
+import emailLogo from '../assets/icons/email.svg'
+import githubLogo from '../assets/icons/github.svg'
+import siteLinkLogo from '../assets/icons/site_link.svg'
 
 function ProjectItem() {
   return (
     <>
-      <div className="project-item">
+      <div className="card project-item" style={{ width: '325px' }}>
         <div className="project-item-tab-rect"></div>
 
-        <section className="project-item-main-rect">
-            <h1>Project Title</h1>
+        <section className="card-body project-item-main-rect">
+            <h1 className="card-title">Project Title</h1>
             <h2>Project Duration</h2>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 
-            <p>Programming Languages / Software Used</p>
+            <p>Programming Languages</p>
             <ul>
-                <li>software</li>
-                <li>software</li>
                 <li>software</li>
                 <li>software</li>
                 <li>software</li>
