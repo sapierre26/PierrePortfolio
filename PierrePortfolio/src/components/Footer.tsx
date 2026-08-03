34749439636acd4f0f265881fa763368f09288e8
Sanaia Pierre <122914227+sapierre26@users.noreply.github.com>
2026-08-03 13:18:41 -0500

    Added styling and formatting to the Footer component

--- PierrePortfolio/src/components/Footer.tsx
+++ PierrePortfolio/src/components/Footer.tsx
@@ -1,9 +1,12 @@
+import '../css/tokens.css'
+import '../css/Footer.css'
+
 function Footer() {
   return (
     <>
-      <footer className="footer">
-        <p>Designed and Developed by Sanaia Pierre | 2026 © All rights reserved.</p>
-      </footer>
+      <div className="card-footer">
+        Designed and Developed by Sanaia Pierre | 2026 © All rights reserved.
+      </div>
     </>
   )
 }
