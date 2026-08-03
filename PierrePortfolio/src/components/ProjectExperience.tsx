80f86fb83c4addaefb67831c2f0b6e29e3487f3b
Sanaia Pierre <122914227+sapierre26@users.noreply.github.com>
2026-08-03 18:50:54 -0500

    Added content to the site (no more placeholder text); Need to do some final responsive formatting fixes and then can deploy the site

--- PierrePortfolio/src/components/ProjectExperience.tsx
+++ PierrePortfolio/src/components/ProjectExperience.tsx
@@ -1,6 +1,51 @@
 import '../css/tokens.css'
 import '../css/ProjectExperience.css'
-import ProjectItem from './ProjectItem.tsx'
+import linkedinLogo from '../assets/icons/linkedin.svg'
+import emailLogo from '../assets/icons/email.svg'
+import githubLogo from '../assets/icons/github.svg'
+import siteLinkLogo from '../assets/icons/site_link.svg'
+
+const featuredProjects = {
+  "Pierre Portfolio": {
+    title: "Pierre Portfolio",
+    duration: "July - August 2026",
+    desc: "This is my online portfolio, designed in Figma and built using Vite, React, and Bootstrap. It’s meant for me to practice my Web Dev skills and showcase the projects I’ve worked on.",
+    techStack: [
+      { item1: "React / Bootstrap",
+        item2: "Vite (frontend)",
+        item3: "HTML / CSS",
+      }
+    ],
+    gitHubLink: "https://github.com/sapierre26/PierrePortfolio",
+    siteLink: "",
+  },
+  "Movie Night Website": {
+    title: "Movie Night Website",
+    duration: "September - December 2025",
+    desc: "A movie library website created to strengthen my skills in Dynamic Web Development. Some features include a genre filter, user login and authentication, and theme switch.",
+    techStack: [
+      { item1: "Vite (frontend) / Express (backend)",
+        item2: "MongoDB Compass",
+        item3: "JavaScript (Node.js) / TypeScript",
+      }
+    ],
+    gitHubLink: "https://github.com/sapierre26/MovieNight",
+    siteLink: "https://movienight-2r9f.onrender.com/",
+  },
+  "Trivia GO! Mobile App": {
+    title: "Trivia GO! Mobile App",
+    duration: "January - April 2026",
+    desc: "A trivia game mobile application created to strengthen my skills in Mobile App Development. Some features include a game timer, varying question times, and score tracking.",
+    techStack: [
+      { item1: "Android Studio",
+        item2: "Kotlin (Java)",
+        item3: "Jetpack Compose",
+      }
+    ],
+    gitHubLink: "https://github.com/sapierre26/TriviaGame",
+    siteLink: "",
+  }
+}
 
 function Projects() {
   return (
@@ -9,9 +54,36 @@ function Projects() {
         <h1>FEATURED PROJECT EXPERIENCE</h1>
 
         <div id="featured-projects">
-          <ProjectItem />
-          <ProjectItem />
-          <ProjectItem />
+          {Object.keys(featuredProjects).map((key) => {
+            const project = featuredProjects[key as keyof typeof featuredProjects]
+
+            return (
+              <div className="card project-item" key={project.title}>
+                <div className="project-item-tab-rect"></div>
+
+                <section className="card-body project-item-main-rect">
+                  <h1 className="card-title">{project.title}</h1>
+                  <h2>{project.duration}</h2>
+
+                  <p className="card-text">{project.desc}</p>
+
+                  <p>Programming Languages</p>
+                  <ul>
+                    <li>{project.techStack[0].item1}</li>
+                    <li>{project.techStack[0].item2}</li>
+                    <li>{project.techStack[0].item3}</li>
+                  </ul>
+
+                  <div className="project-item-links">
+                    <a href="https://www.linkedin.com/in/sanaia-pierre-7104a8207/"><img src={linkedinLogo} alt="Light pink Linkedin logo that navigates to my Linkedin profile" /></a>
+                    <a href="sgpierre2003@gmail.com"><img src={emailLogo} alt="Light pink email logo that navigates to my Gmail" /></a>
+                    <a href={project.gitHubLink}><img src={githubLogo} alt="Light pink GitHub logo that navigates to my GithHub profile" /></a>
+                    <a href={project.siteLink}><img src={siteLinkLogo} alt="Light pink site link logo that navigates to the link for this project, if applicable" /></a>
+                  </div>
+                </section>
+              </div>
+            )
+          })}
         </div>
       </section>
     </>
--- PierrePortfolio/src/components/ResumeExperience.tsx
+++ PierrePortfolio/src/components/ResumeExperience.tsx
@@ -1,6 +1,56 @@
 import '../css/tokens.css'
 import '../css/ResumeExperience.css'
-import ResumeItem from './ResumeItem.tsx'
+
+const featuredExperience = {
+  "Disney College Program": {
+    duration: "Aug. 2026 - PRESENT",
+    location: "Disney College Program",
+    title: "Recent Grad Participant",
+    desc: "Future Tasks: Connected with university students and recent graduates that have a diverse range of studies to strengthen professional network; Attended learning offering classes to obtain personal development skills; Learned about post-program opportunities at Disney and Disney history",
+    skills: [
+      {
+        skill1: "Skill Gained",
+        skill2: "Skill Gained",
+        skill3: "Skill Gained",
+        skill4: "Skill Gained",
+        skill5: "Skill Gained",
+        skill6: "Skill Gained"
+      }
+    ]
+  },
+  "AIP": {
+    duration: "July 2025 - PRESENT",
+    location: "Cal Poly Office of Academic Innovations & Programs (AIP)",
+    title: "Student Web Developer",
+    desc: "Maintained Drupal sites and routinely include up-to-date information, news, and resources for AIP and the AIP sub departments; Used images and graphics to display information and increase site engagement for an otherwise limited existing Cal Poly Drupal template, while following the accessibility compliance test",
+    skills: [
+      {
+        skill1: "Figma",
+        skill2: "Accessibility (WCAG)",
+        skill3: "Drupal (CMS)",
+        skill4: "PHP",
+        skill5: "Adobe Illustrator",
+        skill6: "Canva"
+      }
+    ]
+  },
+  "APP": {
+    duration: "Aug. 2024 - July 2025",
+    location: "Cal Poly Office of Academic Programs & Planning",
+    title: "Student Assistant",
+    desc: "Worked on an archival project by scanning older physical documents into a shared OneDrive folder that were organized by year, college, department, etc; Assisted with the Making Instructional Documents Accessible to Students (MIDAS) project to update Canvas documents, used by professors, to follow modern accessibility guidelines using Equidox",
+    skills: [
+      {
+        skill1: "Microsoft Excel",
+        skill2: "MIcrosoft Word",
+        skill3: "OneDrive",
+        skill4: "Adobe Suite",
+        skill5: "Accessibility (WCAG)",
+        skill6: "Equidox"
+      }
+    ]
+  }
+}
 
 function Resume() {
   return (
@@ -9,9 +59,39 @@ function Resume() {
         <h1>RELEVANT WORK EXPERIENCE</h1>
 
         <div id="featured-experience">
-          <ResumeItem />
-          <ResumeItem />
-          <ResumeItem />
+          {Object.keys(featuredExperience).map((key) => {
+            const experience = featuredExperience[key as keyof typeof featuredExperience]
+
+            return (
+              <div className="card resume-item">
+                <div className="card-body" id="resume-item-body">
+                  <section className="resume-item-years-worked">
+                    <h1 className="card-title">{experience.duration}</h1>
+                  </section>
+
+                  <section className="resume-item-job-description">
+                      <h1 className="card-title">{experience.location}</h1>
+                      <h2>{experience.title}</h2>
+
+                      <br></br>
+
+                      <p className="card-text">{experience.desc}</p>
+
+                      <br></br>
+
+                      <div className="skills-gained-item-gallery">
+                        <div className="item-card"><p>{experience.skills[0].skill1}</p></div>
+                        <div className="item-card"><p>{experience.skills[0].skill2}</p></div>
+                        <div className="item-card"><p>{experience.skills[0].skill3}</p></div>
+                        <div className="item-card"><p>{experience.skills[0].skill4}</p></div>
+                        <div className="item-card"><p>{experience.skills[0].skill5}</p></div>
+                        <div className="item-card"><p>{experience.skills[0].skill6}</p></div>
+                      </div>
+                  </section>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </section>
     </>
